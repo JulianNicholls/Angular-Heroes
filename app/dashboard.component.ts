b6c570ed6d0cc7bc5661817997987c26c444ebcd
Julian Nicholls <juliannicholls29@gmail.com>
2016-04-21 12:48:37 +0100

    Add detail display to Dashboard

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit}    from 'angular2/core';
+import {Router}               from 'angular2/router';
 
 import {Hero}                 from './hero';
 import {HeroService}          from './hero.service';
@@ -8,17 +9,22 @@ import {HeroService}          from './hero.service';
   templateUrl:  `app/dashboard.component.html`
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private _heroService: HeroService) {}
+  constructor(
+    private _router:      Router,
+    private _heroService: HeroService
+  ) {}
 
   ngOnInit() {
     this._heroService.getHeroesSlowly()
       .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
-  gotoDetail() {
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', {id: hero.id}];
 
+    this._router.navigate(link);
   }
 }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,21 +1,30 @@
-import {Component, Input} from 'angular2/core';
-import {Hero}             from './hero';
+import {Component, OnInit}  from 'angular2/core';
+import {RouteParams}        from 'angular2/router';
+
+import {Hero}               from './hero';
+import {HeroService}        from './hero.service';
 
 @Component({
-  selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input type="text" placeholder="name" [(ngModel)]="hero.name" />
-      </div>
-    </div>
-  `
+  selector:     'my-hero-detail',
+  templateUrl:  'app/hero-detail.component.html'
 })
 
-export class HeroDetailComponent {
-  @Input()
-  hero:   Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams
+  ) {}
+
+  ngOnInit() {
+    let id = +this._routeParams.get('id');  // The '+' converts to a number
+
+    this._heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
